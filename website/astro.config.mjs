15ad03257df188f18ae1024d8f06c4ccf3847cd0
Romain Rastel <rromain35@gmail.com>
2024-02-04 14:58:55 +0100

    add the configuration for deploying the website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://letsar.github.io',
 	base: '/state_watcher/',
 	logo: {
 		light: './src/assets/light-logo.svg',
